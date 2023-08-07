4542ff3f5ca4f3f05b901bf73c66e3b21763b735
Nolan <nolannaphys@gmail.com>
2023-08-07 14:01:14 -0400

    fixed package.json, webpack, and sw

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,15 +20,11 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        // title: 'Editor'
       }),
-
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
-
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
